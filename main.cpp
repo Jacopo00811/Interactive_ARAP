746d91b0898623821327437701f4d249267bda32
Jacopo <jacopo.ceccuti@gmail.com>
2024-01-07 12:00:48 +0100

    Added meshes and zip libraries. Updated .gitignore and CMakeLists

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     
-    auto result = (10 <=> 20) > 0;
-    if (result) {
-        std::cout << "10 > 20" << std::endl;
-    } else {
-        std::cout << "10 < 20" << std::endl;
-    }
+    // auto result = (10 <=> 20) > 0;
+    // if (result) {
+    //     std::cout << "10 > 20" << std::endl;
+    // } else {
+    //     std::cout << "10 < 20" << std::endl;
+    // }
 }
